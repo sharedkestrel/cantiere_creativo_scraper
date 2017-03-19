c9074965464ab803e14735adb32e5a2980020049
sharedkestrel <francesco.corazzi@hotmail.it>
2017-03-19 19:55:40 +0100

    Basic version of the scraper

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.cantierecreativo.net/portfolio/"
+portfolio = requests.get(url)
+html = portfolio.text
+soup = BeautifulSoup(html, "html.parser")
+proj_links = soup.find_all("a", class_="works-list__item__link")
+p = open('projects.csv', 'w')
+p.write("Commissioner (Description)\n")
+for a_link in proj_links:
+    link = a_link.get('href')
+    full_link = "https://www.cantierecreativo.net" + link
+    proj_page = requests.get(full_link)
+    proj_html = proj_page.text
+    proj_soup = BeautifulSoup(proj_html, "html.parser")
+    head_soup = proj_soup.find("h1", class_="hero__heading")
+    head = head_soup.get_text()
+    desc_soup = proj_soup.find("h2", class_="hero__text")
+    desc = desc_soup.get_text()
+    data = head + " (" + desc + ")\n"
+    p.write(data)
+p.close()
