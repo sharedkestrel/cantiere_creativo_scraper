a2fbd98d0a494a4a03237c754650f4dbbeb1b48a
sharedkestrel <francesco.corazzi@hotmail.it>
2017-04-06 12:46:20 +0200

    split everything in functions

--- scraper.py
+++ scraper.py
@@ -3,6 +3,17 @@ from bs4 import BeautifulSoup
 import os.path
 
 
+def open_csv(path):
+    try:
+        p = open(os.path.join(path, 'projects.csv'), 'w')
+    except FileNotFoundError:
+        raise FileNotFoundError("The %s folder does not exist." % path)
+    except PermissionError:
+        raise PermissionError("The script doesn't have permission to write in %s" % path)
+    p.write("Commissioner, Description")
+    return p
+
+
 def soupify(site):
     attempts = 0
     # give five attempts to connect to the site, else raise an exception
@@ -22,37 +33,41 @@ def soupify(site):
             attempts += 1
     raise ConnectionError("Connection failed")
 
-# let the user input the file path
-path = input("Insert the path for the csv file: (if none is inserted, the working directory will be used)\n")
-# creates the csv file and writes the first line
-try:
-    p = open(os.path.join(path, 'projects.csv'), 'w')
-except FileNotFoundError:
-    raise FileNotFoundError("The %s folder does not exist." % path)
-except PermissionError:
-    raise PermissionError("The script doesn't have permission to write in %s" % path)
-p.write("Commissioner (Description)")
-url = "https://www.cantierecreativo.net/portfolio/"
-soup = soupify(url)
-# finds every <a> tag with the works-list__item__link class, since they contain the "href"s to the projects
-proj_links = soup.find_all("a", class_="works-list__item__link")
-# for every matching <a> tag, extracts the links and then parses each page
-for a_link in proj_links:
-    link = a_link.get('href')
-    # creates a full link since the site contains local links
+
+def find_elements(soup, tag, clss):
+    elements = soup.find_all(tag, class_=clss)
+    return elements
+
+
+def get_full_link(link):
+    link = link.get('href')
     full_link = "https://www.cantierecreativo.net" + link
-    proj_soup = soupify(full_link)
-    # finds the tag containing the project name and strips out the HTML
-    head_soup = proj_soup.find("h1", class_="hero__heading")
-    head = head_soup.get_text()
-    if ";" in head:
-        head = head.replace(";", "")
-    # finds the tag containing the project description and strips out the HTML
-    desc_soup = proj_soup.find("h2", class_="hero__text")
-    desc = desc_soup.get_text()
-    # joins the two found strings and adds them to a new line in the csv file
+    return full_link
+
+
+def get__text(tag):
+    tag = tag.get_text()
+    if ";" in tag:
+        tag = tag.replace(";", "")
+    return tag
+
+
+def write_data(p, head, desc):
     data = "\n %s (%s)" % (head, desc)
     print("Writing data to csv...\n")
     p.write(data)
+
+
+p = open_csv(input("Insert the path for the csv file: (if none is inserted, the working directory will be used)\n"))
+soup = soupify("https://www.cantierecreativo.net/portfolio/")
+elements = find_elements(soup, "a", "works-list__item__link")
+for link in elements:
+    full_link = get_full_link(link)
+    proj_soup = soupify(full_link)
+    proj_name = find_elements(proj_soup, "h1", "hero__heading")
+    head = proj_name.get__text()
+    proj_desc = find_elements(proj_soup, "h2", "hero__text")
+    desc = proj_desc.get__text()
+    write_data(p, head, desc)
 p.close()
 print("Process complete!")
